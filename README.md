2628ec06f6d68fe9a4dac27f972edb9014ce6426
johnomahoy <53034160+johnomahoy@users.noreply.github.com>
2019-11-11 15:58:04 +0800

    Update README.md

--- README.md
+++ README.md
@@ -4,10 +4,19 @@
 This script will update the contact field for company.
 
 
-These are the expected data from the user:  api\
-                                            key\
-                                            contactId\
+These are the expected data from the user:  api  
+                                            key  
+                                            contactId  
                                             company
-Developed by: Kurl
 
-Check the script for more info.
+(1)Fetch the company name and AccountId in infusionsoft database  
+
+(2)If the value  is not empty.
+    the script will update the account id and company name.
+
+(3)If the value is empty
+    the script will update the account id and company name of the contact base from These
+    data given from the http post.  
+Developed by: Kurl  
+
+Check the script for more info.  
--- company.php
+++ company.php
@@ -1,58 +1,57 @@
 <?php
-//http://scaleupmarketing.asia/httpscripts/kc477/linkcontact/company.php 
+//http://scaleupmarketing.asia/httpscripts/kc477/linkcontact/company.php
 
 //Get the data
 $api = $_REQUEST['app'];
 $key = $_REQUEST['key'];
 function apiLog(){
-	return array($GLOBALS["api"].':'.$GLOBALS["api"].':i:'.$GLOBALS["key"].':This is the connection for '.$GLOBALS["api"].'.infusionsoft.com'); 
+	return array($GLOBALS["api"].':'.$GLOBALS["api"].':i:'.$GLOBALS["key"].':This is the connection for '.$GLOBALS["api"].'.infusionsoft.com');
 }
 
 require_once("isdk.php");
 $app = new iSDK;
 //Test Connection
 if( $app->cfgCon("kc477"))
-{ //echo "You are connected"; 
-	// echo ' You are connected';
+{
 	$contactId = $_REQUEST['contactId'];
-	// $contactId = '9189'; // preset value for testing
+
 	$returnFields = array('Id');
-	$conDat = $app->dsLoad("Contact", $contactId, $returnFields); 
-	// $company = $conDat['FirstName'].$conDat['LastName'];
-	$company = $_REQUEST['company']; 
-	// echo $conDat['Id'];
+	$conDat = $app->dsLoad("Contact", $contactId, $returnFields);
+
+	$company = $_REQUEST['company'];
+
 	$returnFields = array('AccountId','Company');
 	$query = array('Company' => $company);
-	//insert the data into the contact custom fields
+
 	$contacts = $app->dsQuery("Contact",10,0,$query,$returnFields);
-	// print_r($contacts);
-if ($contacts){ //if account eaxis
+
+if ($contacts){
 	foreach ($contacts as $contacts){
 		$compID = $contacts['AccountId'];
 		$compName = $contacts['Company'];
 		break;
 	}
 
-	//Insert into contact
+
 	$conDat = array('AccountId' => $compID, 'Company' => $compName);
 	$conID = $app->updateCon($contactId, $conDat);
-	 
-}else{//Add company to contact
-	$data = array('Company' => $company); 
-	$added = $app->dsAdd("Contact", $data);  
- 	
+
+}else{
+	$data = array('Company' => $company);
+	$added = $app->dsAdd("Contact", $data);
+
 	$returnFields = array('Id');
-	$conData = $app->dsLoad("Contact", $added, $returnFields); 
-	// $company = $conData['FirstName'].$conData['LastName'];
+	$conData = $app->dsLoad("Contact", $added, $returnFields);
+
 	$company = $_REQUEST['company'];
-	//Update the contact
-	$conDat = array('AccountId' => $added, 'Company' => $company); 
-	$conID = $app->updateCon($added, $conDat); 
-	
+
 	$conDat = array('AccountId' => $added, 'Company' => $company);
-	$conID = $app->updateCon($contactId, $conDat); 
- 
+	$conID = $app->updateCon($added, $conDat);
+
+	$conDat = array('AccountId' => $added, 'Company' => $company);
+	$conID = $app->updateCon($contactId, $conDat);
+
 }
 
- 
+
 }
